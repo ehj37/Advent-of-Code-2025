3fb3394a87f1487fe5ffc34948feac2aab98d67a
Evan Johnson <johnsonev207@gmail.com>
2025-12-02 21:18:36 -0800

    Day 3 part 1

---
+++ advent-of-code-2025/day3/day3.gd
@@ -0,0 +1,71 @@
+extends Control
+
+
+class Bank:
+	var batteries: Array[int]
+
+	func _init(bs):
+		batteries = bs
+
+
+var _banks: Array[Bank]
+
+@onready var button_part_1: Button = $VBoxContainer/VBoxContainerPart1/ButtonPart1
+@onready var label_part_1: Label = $VBoxContainer/VBoxContainerPart1/LabelPart1
+@onready var button_part_2: Button = $VBoxContainer/VBoxContainerPart2/ButtonPart2
+@onready var label_part_2: Label = $VBoxContainer/VBoxContainerPart2/LabelPart2
+
+
+func _parse_input() -> void:
+	if !_banks.is_empty():
+		return
+
+	var file = FileAccess.open("res://day3/input.txt", FileAccess.READ)
+	while !file.eof_reached():
+		var line = file.get_line()
+		print(line)
+		if line.is_empty():
+			continue
+
+		var batteries: Array[int] = []
+		for c in line.split(""):
+			batteries.append(int(c))
+
+		_banks.append(Bank.new(batteries))
+
+
+func part_one() -> String:
+	_parse_input()
+
+	var joltage_sum: int = 0
+	for bank in _banks:
+		var sorted_batteries = bank.batteries.duplicate()
+		sorted_batteries.sort()
+		sorted_batteries.reverse()
+		var highest_joltage = sorted_batteries[0]
+		var highest_joltage_i = bank.batteries.find(highest_joltage)
+		if highest_joltage_i < bank.batteries.size() - 1:
+			joltage_sum += highest_joltage * 10 + bank.batteries.slice(highest_joltage_i + 1).max()
+		else:
+			var second_highest_joltage = sorted_batteries[1]
+			joltage_sum += second_highest_joltage * 10 + highest_joltage
+
+	return str(joltage_sum)
+
+
+func part_two() -> String:
+	return "Implement me!"
+
+
+func _on_button_part_1_pressed():
+	button_part_1.disabled = true
+	label_part_1.text = "Calculating…"
+	label_part_1.text = part_one()
+	button_part_1.disabled = false
+
+
+func _on_button_part_2_pressed():
+	button_part_2.disabled = true
+	label_part_2.text = "Calculating…"
+	label_part_2.text = part_two()
+	button_part_2.disabled = false
